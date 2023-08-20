bf7e2c47a68f28106471e5bdf0470eccb357f80f
ShaiShaike <shai310@gmail.com>
2023-08-20 12:29:03 +0300

    bug fix

--- SceneSketch/models/video_painter.py
+++ SceneSketch/models/video_painter.py
@@ -12,7 +12,7 @@ import sketch_utils as utils
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from painter_params import Painter
+from models.painter_params import Painter
 
 
 class VideoPainter(Painter):
