be8a2a62e2ee1a8ec6984fcb71c910cc8e22497b
ShaiShaike <shai310@gmail.com>
2023-08-25 01:22:44 +0300

    video_processing saving video

--- video_preprocess.py
+++ video_preprocess.py
@@ -6,6 +6,7 @@ from os import makedirs
 def prep_video(input_path: str, output_dir: str, start_frame: int, end_frame: int,
                resize_to: int, crop=None):
     cap = cv2.VideoCapture(input_path)
+    
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     assert start_frame >= 0 and start_frame < end_frame and end_frame < frame_count
@@ -16,6 +17,16 @@ def prep_video(input_path: str, output_dir: str, start_frame: int, end_frame: in
 
     cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
     success,image = cap.read()
+    
+    if crop is not None:
+        h_start, h_end, w_start, w_end = crop
+        temp = image[h_start:h_end, w_start:w_end]
+    im_size = temp.shape[:2]
+    resize_scale = resize_to / max(im_size)
+    size = tuple([int(dim * resize_scale) for dim in im_size[::-1]])
+
+    outcrop = cv2.VideoWriter(r"C:\projects\VideoSketch\preped_videos\ballerina.mp4", -1,
+                              cap.get(cv2.CAP_PROP_FPS), size)
     for count, _ in enumerate(range(start_frame, end_frame)):
         if crop is not None:
             h_start, h_end, w_start, w_end = crop
@@ -23,9 +34,13 @@ def prep_video(input_path: str, output_dir: str, start_frame: int, end_frame: in
         im_size = image.shape[:2]
         resize_scale = resize_to / max(im_size)
         image = cv2.resize(image, [int(dim * resize_scale) for dim in im_size[::-1]])
+        outcrop.write(image)
         cv2.imwrite(str(output_dir / f"frame{count}.png"), image)     # save frame as JPEG file      
         success, image = cap.read()
         print('Read a new frame: ', success)
+    
+    cap.release()
+    outcrop.release()
 
 if __name__ == "__main__":
     vid_path = r'C:\projects\VideoSketch\videos\production_id_4990428 (1080p).mp4'
