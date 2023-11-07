39a7b7d2eb7acc24d1936a275bc78ad35f11e11b
ShaiShaike <shai310@gmail.com>
2023-11-07 18:40:44 +0200

    bug fix

--- SceneSketch/sketch_utils.py
+++ SceneSketch/sketch_utils.py
@@ -535,7 +535,8 @@ def inference_video(args, eps=1e-4, is_video=0):
     checkpoint = torch.load(mlp_points_weights_path)
     mlp.load_state_dict(checkpoint['model_state_dict'])
 
-    motion_mlp = MotionMLP(num_strokes=num_paths, num_cp=control_points_per_seg).to(device)
+    motion_mlp = MotionMLP(num_strokes=num_paths, num_cp=control_points_per_seg,
+                           num_pos_encoding=args.num_pos_encoding, device=device).to(device)
     checkpoint = torch.load(mlp_motion_weights_path)
     motion_mlp.load_state_dict(checkpoint['model_state_dict'])
 
